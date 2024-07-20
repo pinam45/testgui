8cd20afc389f24ac204183d1a1348a521733ec21
Maxime Pinard <maxime.pin@live.fr>
2024-07-20 22:32:36 +0200

    Removed Hello world window

--- src/main.cpp
+++ src/main.cpp
@@ -570,30 +570,6 @@ int main(int argc, char* argv[])
             font::pop();
         }
 
-        // "Hello, world!" window
-        {
-            ImGui::Begin("Hello, world!");// Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_imgui_demo_window);// Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            static float f = 0.0f;
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*) &clear_color);// Edit 3 floats representing a color
-
-            static int counter = 0;
-            if(ImGui::Button("Button"))
-            {
-                counter++;
-            }
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
         // "Another Window" window
         if(show_another_window)
         {
